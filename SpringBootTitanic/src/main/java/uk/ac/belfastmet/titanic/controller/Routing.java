f962f9f32a9a2bbdb998ef2671af0ec4047314c9
Conor Pointon (POI18161670) <POI18161670@belfastmet.ac.uk>
2018-12-04 15:14:59 +0000

    Init titanic commit

--- SpringBootTitanic/src/main/java/uk/ac/belfastmet/titanic/controller/Routing.java
+++ SpringBootTitanic/src/main/java/uk/ac/belfastmet/titanic/controller/Routing.java
@@ -1,17 +1,51 @@
 package uk.ac.belfastmet.titanic.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import uk.ac.belfastmet.titanic.repository.PassengerRepository;
+
+@Controller
+@RequestMapping("")
 public class Routing {
+	PassengerRepository passengerRepository;
 	
-	Routing() {
+	Routing(PassengerRepository passengerRepository) {
 		super();
+		this.passengerRepository = passengerRepository;
 	}
 	
 	@GetMapping("")
 	public String home(Model model) {
-		return "index";
+		model.addAttribute("pageTitle", "Titanic");
+		
+		return "index.html";
+	}
+	
+	@GetMapping("/passengers")
+	public String passengers(Model model) {
+		model.addAttribute("pageTitle", "Passenegrs");
+		model.addAttribute("passengers", passengerRepository.findAll());
+		
+		return "passengers";
+	}
+	
+	public String passengers(@RequestParam("search") String search, @RequestParam("category") String category, Model model) {
+		model.addAttribute("pageTitle", "Passenegrs");
+		if(search.equals(null) || category.equals(null))
+			model.addAttribute("passengers", passengerRepository.findAll());
+		
+		switch(category) {
+			case "ID": model.addAttribute("passengers", passengerRepository.findByPassengerId(Integer.parseInt(search))); break;
+			case "Name": model.addAttribute("passengers", passengerRepository.findByName(search)); break;
+			case "Age": model.addAttribute("passengers", passengerRepository.findByAge(search)); break;
+			case "Survived": model.addAttribute("passengers", passengerRepository.findBySurvived(Integer.parseInt(search))); break;
+		}
+		
+		return "passengers";
 	}
 
 }
---
+++ SpringBootTitanic/src/main/java/uk/ac/belfastmet/titanic/repository/PassengerRepository.java
@@ -0,0 +1,17 @@
+package uk.ac.belfastmet.titanic.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import uk.ac.belfastmet.titanic.domain.Passenger;
+
+public interface PassengerRepository extends CrudRepository<Passenger, Integer> {
+
+	Passenger findByPassengerId(int parseInt);
+
+	Passenger findByName(String search);
+
+	Iterable<Passenger> findByAge(String search);
+
+	Iterable<Passenger> findBySurvived(int parseInt);
+	
+}
